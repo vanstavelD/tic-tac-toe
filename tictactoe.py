b4e9b33e3b4442f866d4cdc9d5a3eb97976ea195
Dylan Vanstavel <vanstavel.dylan@outlook.fr>
2023-01-12 09:55:18 +0100

    commencement, création des premières variables

---
+++ tictactoe.py
@@ -0,0 +1,20 @@
+# pour commencer il faut créer des variables qui vont nous aider pour la suite
+grille = ["-", "-", "-",
+          "-", "-", "-",
+          "-", "-", "-"]
+joueur_un = "X"
+gagnant = None
+jeu_en_cours = True
+
+# faire la grille
+# def afficher_grille(grille):
+#     print(grille[0] + " | " + grille[1] + " | " + grille[2])
+#     print(grille[3] + " | " +)
+
+# le coup d'un joueur
+
+# verifier gagnant ou nul
+
+# Changement de joueur
+
+# verifier gagnant ou nul à nouveau
