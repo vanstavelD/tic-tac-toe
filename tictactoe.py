455034dfbaa842572d64abf185123b5e1afcc344
Dylan Vanstavel <vanstavel.dylan@outlook.fr>
2023-01-12 10:23:28 +0100

    docstring de la fonction afficher grille

--- tictactoe.py
+++ tictactoe.py
@@ -8,14 +8,20 @@ jeu_en_cours = True
 
 # faire la grille
 def afficher_grille(grille):
+    """ Fonction qui affiche une grille de Tic Tac Toe composée de 9 cases.
+
+    Args:
+        grille (liste): Remplir votre liste de 9 éléments de type str, les 9 éléments sont les 9 cases de la grille du tic Tac Toe
+    """
     print(grille[0] + " | " + grille[1] + " | " + grille[2])
     print("----------")
     print(grille[3] + " | " + grille[4] + " | " + grille[5])
     print("----------")
     print(grille[6] + " | " + grille[7] + " | " + grille[8])
-
+    
 # le coup d'un joueur
-
+def entrer_joueur(grille):
+    
 # verifier gagnant ou nul
 
 # Changement de joueur
