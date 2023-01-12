425494dc62996fb97ba70e459a715a1fed2d1c11
Dylan Vanstavel <vanstavel.dylan@outlook.fr>
2023-01-12 10:05:07 +0100

    création de la grille

--- tictactoe.py
+++ tictactoe.py
@@ -7,9 +7,12 @@ gagnant = None
 jeu_en_cours = True
 
 # faire la grille
-# def afficher_grille(grille):
-#     print(grille[0] + " | " + grille[1] + " | " + grille[2])
-#     print(grille[3] + " | " +)
+def afficher_grille(grille):
+    print(grille[0] + " | " + grille[1] + " | " + grille[2])
+    print("----------")
+    print(grille[3] + " | " + grille[4] + " | " + grille[5])
+    print("----------")
+    print(grille[6] + " | " + grille[7] + " | " + grille[8])
 
 # le coup d'un joueur
 
