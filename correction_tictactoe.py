702890a8c167738219d0b78d3d13213d3a1a953a
Dylan Vanstavel <vanstavel.dylan@outlook.fr>
2023-01-16 10:30:32 +0100

    Fonction verifier coup

--- correction_tictactoe.py
+++ correction_tictactoe.py
@@ -24,3 +24,37 @@ def afficher_grille(plateau:dict) -> None:
                 print(element, end="\t|\t")
         print("\n---------------------------------------------------------")
         
+def jouer_coup(plateau:dict, joueur:str, coup:str) -> None:
+    """Fonction qui joue un coup(Ne vérifie pas si le coup est valide)
+
+    Args:
+        plateau (dict): Le plateau de jeu
+        joueur (str): "O" ou "X"
+        coup (str): Coordonnées de la forme "A1"
+    """
+    plateau[coup[0]][int(coup[1])] = joueur
+
+def verifier_coup(plateau:dict, coup:str) -> bool:
+    """Fonction qui vérifie si un coup est valide
+
+    Args:
+        plateau (dict): plateau de jeu
+        coup (str): un coup de la forme "A1" avec le premier caractere entre A et C et entre 0 et 2
+
+    Returns:
+        bool: True si le coup est valide, False sinon
+    """
+    #On vériefie si le coup est une case qui existe
+    if coup[0].upper() not in ["A", "B", "C"]:
+        return False
+    if coup[1] not in ["0", "1", "2"]:
+        return False
+    
+    #On vérifie si la case est vide
+    if plateau[coup[0].upper()][int(coup[1])] == None:
+        return True
+    else:
+        return False
+    
+    
+    
